7933b128c7d0ce5512ac37b52ea6b4255178d38a
ZeroDyna <renato98990@gmail.com>
2023-03-22 16:56:23 -0500

    hola mundo con Cmake

---
+++ 1/build/CMakeFiles/hola.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hola.dir/hola.cpp.o"
+  "CMakeFiles/hola.dir/hola.cpp.o.d"
+  "hola"
+  "hola.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hola.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
